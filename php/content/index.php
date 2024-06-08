fe2c53062dd5607a0cf5e3906a015e2d13f66026
bobagby78 <bobagby78@gmail.com>
2024-06-08 08:20:56 -0500

    start updating site for job search, add tailwind

---
+++ php/content/index.php
@@ -0,0 +1,31 @@
+<?php
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../../assets/styles.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js" integrity="sha384-BBtl+eGJRgqQAUMxJ7pMwbEyER4l1g+O15P+16Ep7Q9Q+zqX6gSbd85u4mG4QzX+" crossorigin="anonymous"></script>
+    <title>jonbbagby</title>
+</head>
+
+<body>
+    <div class="container my-4">
+        <div class="header">
+            <p class="center big-n-bold" style="margin-left: auto">Jon B Bagby</p>
+            <p class="center med-n-bold"> Full Stack Web Developer</p>
+        </div>
+        <div class="row"></div>
+        <div class="row pretty-box">
+            <div class="col-6 left image">picture goes here</div>
+            <div class="col-6 right words">content goes here</div>
+
+        </div>
+    </div>
+</body>
+
+</html>
