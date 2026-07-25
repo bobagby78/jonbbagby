4d48b059be52dd6e4bde38042b91337bd9986f9d
bobagby78 <bobagby78@gmail.com>
2026-07-25 13:20:35 -0500

    add a period

--- index.php
+++ index.php
@@ -12,7 +12,7 @@
 
 <body class="main-body" background="assets/images/brkbkKayak.jpg">
     <div id="main-container">
-        <h1 class="main-heading center">Hey, I'm Jon and I'm a developer</h1>
+        <h1 class="main-heading center">Hey, I'm Jon and I'm a developer.</h1>
     </div>
 </body>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
