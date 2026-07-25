9a6d08ad6f7f781ee79a15dd8141df6cb9467fbf
bobagby78 <bobagby78@gmail.com>
2026-07-25 13:19:56 -0500

    basic bg and layout started

--- index.php
+++ index.php
@@ -6,12 +6,13 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <link rel="stylesheet" href="./assets/css/styles.css">
+    <link rel="icon" type="image/x-icon" href="assets/images/favicon.ico">
     <title>jbbagby</title>
 </head>
 
-<body>
+<body class="main-body" background="assets/images/brkbkKayak.jpg">
     <div id="main-container">
-        <h1>Welcome to jbbagby.com</h1>
+        <h1 class="main-heading center">Hey, I'm Jon and I'm a developer</h1>
     </div>
 </body>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
